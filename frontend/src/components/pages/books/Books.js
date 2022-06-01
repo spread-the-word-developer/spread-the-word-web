ef3de795b67123aec7bb2e0177bfe2d67c54cf46
Nicholas Iyamabo <niyamabo@gmail.com>
2022-06-02 00:10:10 +0100

    search frontend for books page

--- frontend/src/components/pages/books/Books.js
+++ frontend/src/components/pages/books/Books.js
@@ -4,7 +4,9 @@ import style from './Books.module.css'
 import { Link } from 'react-router-dom'
 
 
-
+const initialValues = {
+    input_text: "",
+};
 function Books(props) {
 
     const[Cards, setCards] = useState([])
@@ -12,17 +14,13 @@ function Books(props) {
 
     useEffect(()=>{
         async function fetchCards(){
-            const req = await axios.post('/api/publication/get-books-query',{search_query: "testbook4", page_number: 1});
+            const req = await axios.post('/api/publication/get-books-query',{search_query: "testbook6", page_number: 1, sort_by: sort_type});
             console.log(req);
             var cardDicArray = [];
-            var max = req.data.length
-            //if max > 5 then set max to 5
-            if (max > 5){
-                max = 5;
-            }
+         
             
-            for (var i = 0 ; i < max; i++){
-                if ((props.dontInclude === null) || (props.dontInclude != req.data[i]._id)) {
+            for (var i = 0 ; i < req.data.length; i++){
+                
                     var cardDic = {};
                     cardDic.id = req.data[i]._id;
                     cardDic.bookName = req.data[i].title;
@@ -31,15 +29,7 @@ function Books(props) {
                     cardDic.authorName = authorNames;
                     cardDic.thumbnailLink = req.data[i].thumbnailLink;
                     cardDicArray.push(cardDic);
-                }
-            
-                else{
-                    if (props.dontInclude != null){
-                        if(max++ > req.data.length){
-                            max++;
-                        }
-                    }
-                }
+               
             }
             console.log("card dic: ",cardDicArray)
             await setCards(cardDicArray);
@@ -68,15 +58,101 @@ function Books(props) {
        const response = await axios.post('/api/publication/create');
    }
 
+   
+   
+   const [sort_type, setSort] = useState("relevance");
+
+   const [search_query, setQuery] = useState(initialValues);
+   const handleInputChange = (e) => {
+       const { name, value } = e.target;
+       setQuery({
+           ...search_query.input_text,
+           [name] : value,
+       });
+       console.log(search_query)
+   };
+
+   const handleChangeSelect = (e) => {
+       setSort(e.target.value);
+    
+   }
+
+   const handleSubmit = (e) => {
+       console.log(search_query.search_term , sort_type)
+       searchPage(search_query.search_term,sort_type, 1 )
+
+   }
+   const searchPage = async(search , sort, page_number) => {
+        if (search === ""){
+            search = null;
+        }
+        //update books
+        const req = await axios.post('/api/publication/get-books-query',{search_query: search, page_number: page_number, sort_by: sort});
+        console.log(req);
+        var cardDicArray = [];
+
+
+        for (var i = 0 ; i < req.data.length; i++){
+
+            var cardDic = {};
+            cardDic.id = req.data[i]._id;
+            cardDic.bookName = req.data[i].title;
+            const authorNames =  await getAuthor(req.data[i].authorID);
+            console.log("author name: ",authorNames);
+            cardDic.authorName = authorNames;
+            cardDic.thumbnailLink = req.data[i].thumbnailLink;
+            cardDicArray.push(cardDic);
+
+        }
+        console.log("card dic: ",cardDicArray)
+        await setCards(cardDicArray);
+
+   }
+
     return (
+
+        <>
+        
+
         <div lassName= {style.cards_container_container}>
 
+        <div className= {style.search_container}>
+            <div className={style.inner_container}>
+            <div className={style.form}>
+                <input 
+                    className = {style.textbox}
+                    type="search"
+                    id = "search_term"
+                    name = "search_term"
+                    value = {search_query.input_text}
+                    onChange = {handleInputChange}
+                    placeholder = "">
+                    
+
+                </input>
+            </div>
+            <div >
+                <select className={style.dopdown} value = {sort_type} onChange = {handleChangeSelect} id="sort_by">
+                    <option value="relevance">relevance</option>
+                    <option value="price">price</option>
+                    <option value="upload_date">upload date</option>
+                    
+                </select>
+            </div>
+            <div className={style.submit_button}>
+                <button className = {style.submit_butn} onClick = {handleSubmit} >
+                    SEARCH
+                </button>
+            </div>
+        </div>
+        </div>
         <div className= {style.cards_container}>
             
             {Cards.map((card) => {
                
                 return(
         
+
                       <div className={style.card} >
             <Link className = {style.link} to = {"/publication/"+card.id}  >
             <div className= {style.card_inner}>
@@ -101,6 +177,7 @@ function Books(props) {
             })}
         </div>
         </div>
+        </ >
     );
 }
 
